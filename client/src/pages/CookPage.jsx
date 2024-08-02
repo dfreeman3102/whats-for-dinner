b7c9127b4a286ef37acd1786d294def1cfe41c0c
dfreeman3102 <davidfreeman3102@gmail.com>
2024-08-02 18:51:22 -0400

    added an input for at home meals that then displays a list, need to add DB functionality to store saved meals

--- client/src/pages/CookPage.jsx
+++ client/src/pages/CookPage.jsx
@@ -1,9 +1,35 @@
-import Spinner from "../components/Spinner"
+import Spinner from "../components/Spinner";
+import { useState } from "react";
 export default function CookPage() {
-    return (
-        <div>
-            <h2>Cook at Home</h2>
-        <Spinner />
-        </div>
-    )
-    }
+  const [meals, setMeals] = useState([]);
+  const [addedMeal, setAddedMeal] = useState("");
+  const addMeal = () => {
+    if(addedMeal === "") return;
+    setMeals([...meals, addedMeal]);
+    setAddedMeal("");
+  };
+  return (
+    <div>
+      <h2>Cook at Home</h2>
+
+      <h3>What meals can you cook?</h3>
+
+      <input
+        type="text"
+        value={addedMeal}
+        onChange={(e) => setAddedMeal(e.target.value)}
+        placeholder="Enter Meals Here"
+      />
+
+      <button onClick={addMeal}>Add</button>
+
+      <ul>
+        {meals.map((meal, index) => (
+          <li key={index}>{meal}</li>
+        ))}
+      </ul>
+
+      <Spinner />
+    </div>
+  );
+}
