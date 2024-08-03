001fce539dc2d4909ea85d6ac0eb71c22ac6839a
dfreeman3102 <davidfreeman3102@gmail.com>
2024-08-03 15:04:07 -0400

    more fixes

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,20 +15,25 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-  
+
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  
+
+  // Use the Apollo Server middleware
   app.use('/graphql', expressMiddleware(server));
 
+  // Serve static files in production
   if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the React app
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
+    // Handle React routing, return all requests to the React app
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
     });
-  } 
+  }
 
+  // Start the database and then the server
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
