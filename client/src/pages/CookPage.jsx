7f4160ae973a9b34224f0d121802cf9a57277384
dfreeman3102 <davidfreeman3102@gmail.com>
2024-08-18 19:09:55 -0400

    added the savedmealslist onto the cookpage and authentication to view and use cookpage properly

--- client/src/pages/CookPage.jsx
+++ client/src/pages/CookPage.jsx
@@ -1,10 +1,22 @@
 import Spinner from "../components/Spinner";
+import SavedMealsList from "../components/savedMealsList";
+
+import Auth from "../utils/auth";
+
 export default function CookPage() {
- 
+  if (!Auth.loggedIn()) {
+    return (
+      <div>
+        <h2>Cook at Home</h2>
+        <p>You must be logged in to use this page</p>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>Cook at Home</h2>
-      <p>To edit your saved meals, go to Settings!</p>
+      <SavedMealsList />
       <Spinner />
     </div>
   );
